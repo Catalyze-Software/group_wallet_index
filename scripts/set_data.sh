6ba7c0a574c8b7d150695fe22582f57c3dbad486
Remco <remco@r3m.codes>
2024-06-06 15:57:22 +0200

    add set data script

--- scripts/set_data.sh
+++ scripts/set_data.sh
@@ -1,10 +1,13 @@
-# # upload scalable wasm
-# FILE=files/tos.txt
-# CANISTER=o7ouu-niaaa-aaaap-ahhdq-cai
-# IDENTITY=$(dfx identity whoami)
+# catalyze_production
 
-# ic-repl -r ic << END
-# identity ${IDENTITY} "~/.config/dfx/identity/${IDENTITY}/identity.pem"
-# import controller = "${CANISTER}" as "candid/static_files.did"
-# call controller.upload_document(variant{TermsOfService=file("${FILE}")})
-# END
+# upload scalable wasm
+FILE=scripts/multisig.wasm.gz
+CANISTER=o7ouu-niaaa-aaaap-ahhdq-cai
+IDENTITY=$(dfx identity whoami)
+
+ic-repl -r ic << END
+identity ${IDENTITY} "~/.config/dfx/identity/${IDENTITY}/identity.pem"
+import controller = "${CANISTER}" as "candid/wallet_index.did"
+call controller._dev_upload_multisig_wasm(file("${FILE}"))
+call controller._dev_set_proxy(principal "onidn-byaaa-aaaap-ahhaq-cai")
+END
