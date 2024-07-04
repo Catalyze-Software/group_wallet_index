e22b2e51182d9108a57503f7ed89d441141180ca
Remco <remco@r3m.codes>
2024-07-04 11:12:26 +0200

    Update propose upgrade script

--- scripts/propose_upgrade.sh
+++ scripts/propose_upgrade.sh
@@ -6,8 +6,7 @@ PEM_FILE="$(readlink -f "$HOME/.config/dfx/identity/${OWNER_IDENTITY}/identity.p
 DEVELOPER_NEURON_ID="37173462e82235788f2592e076b31cf0e8601eff16b2a8687b564589d867de36"
 
 VERSION=$(grep '^version = ' src/Cargo.toml | sed -E 's/version = "(.*)"/\1/')
-CHANGELOG="Minor bug fixes and improvements."
-# CHANGELOG=$(sed -n '/## \['"$VERSION"'\]/,/^## \[/p' CHANGELOG.md | sed '$d')
+CHANGELOG=$(sed -n "/## \[${VERSION}\]/,/^## \[/p" CHANGELOG.md | sed '$d')
 WASM_PATH="wasm/wallet_index.wasm.gz"
 
 bash scripts/build.sh
